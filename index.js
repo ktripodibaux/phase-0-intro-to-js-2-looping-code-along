e6ef1cb74b062618fbce3aa42511316c41ff9b05
Kurt Baux <ktripodibaux@gmail.com>
2022-06-08 12:32:38 -0400

    commit tree 12803be85a6de9302db53d688915ff87f0d82eff

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(arr, event) {
+    const newArr = [];
+    for (let name in arr) {
+        newArr.push(`Thank you, ${arr[name]}, for the wonderful ${event} gift!`);
+    }
+    return newArr;
+}
+
+function countDown(number) {
+    for(let count = number; count > -1; count--){
+        console.log(count);
+    }
+}
